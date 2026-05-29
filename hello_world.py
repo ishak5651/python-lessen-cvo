2bd5440e581241d4a5a29271895e2f13b7d46b8f
ishak5651 <ishakeddarkaoui@gmail.com>
2026-05-29 11:14:34 +0200

    oefB klaar

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+tekst_a = "PYTHON"
+print(tekst_a.lower())  
+
+tekst_b = "hallo"
+print(tekst_b.strip())  
+
+tekst_c = "oude auto"
+print(tekst_c.replace("oude", "nieuwe"))  
+
+
+tekst_d = "Python"
+print(len(tekst_d))
