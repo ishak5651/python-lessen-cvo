7701ca067fa5363f721834e468dc162652bb3801
ishak5651 <ishakeddarkaoui@gmail.com>
2026-05-29 11:41:48 +0200

    oefC deel1 klaar

---
+++ les2_oefC.py
@@ -0,0 +1,15 @@
+getal1_tekst = input("Voer het eerste getal in:")
+getal2_tekst = input("Voer het tweede getal in:")
+
+getal1 = int(getal1_tekst)
+getal2 = int(getal2_tekst)
+
+som = getal1 + getal2
+verschil = getal1 - getal2
+product = getal1 * getal2
+deling = getal1 / getal2
+
+print(f"som: {som}")
+print(f"verschil: {verschil}")
+print(f"product: {product}")
+print(f"deling: {deling}")
