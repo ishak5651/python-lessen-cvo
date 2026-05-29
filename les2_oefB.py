fd76fef4e511129be2b0ebc272e9e68d9581f9d6
ishak5651 <ishakeddarkaoui@gmail.com>
2026-05-29 11:23:41 +0200

    oefB klaar

--- les2_oefB.py
+++ les2_oefB.py
@@ -4,3 +4,8 @@ Leeftijd = int(Leeftijd_tekst)
 Volgend_jaar = Leeftijd + 1
 print(f"Hallo{Naam}! Volgend jaar ben je {Volgend_jaar} jaar oud")
 print(Naam.upper())
+
+print(Naam.lower())  
+print(Naam.strip())  
+print(Naam.replace("oude", "nieuwe"))  
+print(len(Naam))
