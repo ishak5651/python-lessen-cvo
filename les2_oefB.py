bd73c2298c6538d4ed9df028bdc23fb513194042
ishak5651 <ishakeddarkaoui@gmail.com>
2026-05-29 10:51:01 +0200

    oefB klaar

---
+++ les2_oefB.py
@@ -0,0 +1,6 @@
+Voornaam = input("Wat is je voornaam?")
+Leeftijd_tekst = input("Wat is je leeftijd")
+Leeftijd = int(Leeftijd_tekst)
+Volgend_jaar = Leeftijd + 1
+print(f"Hallo{Voornaam}! Volgend jaar ben je {Volgend_jaar} jaar oud")
+print(Voornaam.upper())
